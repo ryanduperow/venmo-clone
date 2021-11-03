15316dc5ee4d3fab3ae4943fc3506ca2c5b2530e
Levi Swartz <levi@leviswartz.net>
2021-11-03 15:52:25 -0400

    Complete work on TransferSqlDao

--- dotnet/TenmoServer/DAO/ITransferDao.cs
+++ dotnet/TenmoServer/DAO/ITransferDao.cs
@@ -8,7 +8,7 @@ namespace TenmoServer.DAO
 {
     public interface ITransferDao
     {
-        public void TransferMoney(int fromUserId, int toUserId, decimal transferAmount);
+        public void TransferMoney(int fromUserId, int toUserId, int transferTypeId, decimal transferAmount);
         public IList<Transfer> GetAllUserTransfers(int userId);
         public Transfer GetTransferById(int transferId);
     }
--- dotnet/TenmoServer/DAO/TransferSqlDao.cs
+++ dotnet/TenmoServer/DAO/TransferSqlDao.cs
@@ -18,6 +18,10 @@ namespace TenmoServer.DAO
         {
             List<Transfer> listOfTransfers = new List<Transfer>();
 
+            //TODO: Call this only once or not at all
+            UserSqlDao userSqlDao = new UserSqlDao(connectionString);
+            int accountID = userSqlDao.GetUserAccountID(userId);
+
             try
             {
                 using (SqlConnection conn = new SqlConnection(connectionString))
@@ -25,13 +29,13 @@ namespace TenmoServer.DAO
                     conn.Open();
 
                     SqlCommand cmd = new SqlCommand("SELECT transfer_id, transfer_type_id, transfer_status_id, account_from, account_to, amount FROM transfers WHERE account_from = @account_id OR account_to = @account_id", conn);
-                    cmd.Parameters.AddWithValue("@account_id", );
+                    cmd.Parameters.AddWithValue("@account_id", accountID);
                     SqlDataReader reader = cmd.ExecuteReader();
 
                     while (reader.Read())
                     {
-                        User u = GetUserFromReader(reader);
-                        returnUsers.Add(u);
+                        Transfer currentTransfer = GetTransferFromReader(reader);
+                        listOfTransfers.Add(currentTransfer);
                     }
                 }
             }
@@ -40,17 +44,73 @@ namespace TenmoServer.DAO
                 throw;
             }
 
-            return returnUsers;
+            return listOfTransfers;
         }
 
         public Transfer GetTransferById(int transferId)
         {
-            throw new NotImplementedException();
+            Transfer transfer = new Transfer();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT transfer_id, transfer_type_id, transfer_status_id, account_from, account_to, amount FROM transfers WHERE transfer_id = transfer_id;", conn);
+                    cmd.Parameters.AddWithValue("@transfer_id", transferId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        transfer = GetTransferFromReader(reader);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return transfer;
         }
 
-        public void TransferMoney(int fromUserId, int toUserId, decimal transferAmount)
+        public void TransferMoney(int fromUserId, int toUserId, int transferTypeId, decimal transferAmount)
         {
-            throw new NotImplementedException();
+            UserSqlDao userSqlDao = new UserSqlDao(connectionString);
+            int fromAccountID = userSqlDao.GetUserAccountID(fromUserId);
+            int toAccountID = userSqlDao.GetUserAccountID(toUserId);
+         
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    //TODO: if they ever change what transfer status 1 means, this will break
+                    SqlCommand cmd = new SqlCommand("INSERT INTO transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount) VALUES(@transfer_type_id, 1, @account_from, @account_to, @amount); ", conn);
+                    cmd.Parameters.AddWithValue("@transfer_type_id", transferTypeId);
+                    cmd.Parameters.AddWithValue("@account_from", fromAccountID);
+                    cmd.Parameters.AddWithValue("@account_to", toAccountID);
+                    cmd.Parameters.AddWithValue("@amount", transferAmount);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+        }
+
+        private Transfer GetTransferFromReader(SqlDataReader reader)
+        {
+            return new Transfer()
+            {
+                TransferID = Convert.ToInt32(reader["transfer_id"]),
+                TransferTypeID = Convert.ToInt32(reader["transfer_type_id"]),
+                TransferStatusID = Convert.ToInt32(reader["transfer_status_id"]),
+                AccountFrom = Convert.ToInt32(reader["account_from"]),
+                AccountTo = Convert.ToInt32(reader["account_to"]),
+                Amount = Convert.ToDecimal(reader["amount"]),
+            };
         }
     }
 }
