793f6583dd4d88eae04ddb51942c6204352776c5
Levi Swartz <levi@leviswartz.net>
2021-11-03 15:31:10 -0400

    Create Transfer model; begin work on TransferSqlDao

---
+++ dotnet/TenmoServer/DAO/ITransferDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.Models;
+
+namespace TenmoServer.DAO
+{
+    public interface ITransferDao
+    {
+        public void TransferMoney(int fromUserId, int toUserId, decimal transferAmount);
+        public IList<Transfer> GetAllUserTransfers(int userId);
+        public Transfer GetTransferById(int transferId);
+    }
+}
---
+++ dotnet/TenmoServer/DAO/TransferSqlDao.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using TenmoServer.Models;
+
+namespace TenmoServer.DAO
+{
+    public class TransferSqlDao : ITransferDao
+    {
+        private readonly string connectionString;
+
+        public TransferSqlDao(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public IList<Transfer> GetAllUserTransfers(int userId)
+        {
+            List<Transfer> listOfTransfers = new List<Transfer>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT transfer_id, transfer_type_id, transfer_status_id, account_from, account_to, amount FROM transfers WHERE account_from = @account_id OR account_to = @account_id", conn);
+                    cmd.Parameters.AddWithValue("@account_id", );
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        User u = GetUserFromReader(reader);
+                        returnUsers.Add(u);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return returnUsers;
+        }
+
+        public Transfer GetTransferById(int transferId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TransferMoney(int fromUserId, int toUserId, decimal transferAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
