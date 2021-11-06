e55801cc7739d571d352adfe4526d866f457f26b
Levi Swartz <levi@leviswartz.net>
2021-11-06 09:28:49 -0400

    Added field requirements to the Transfer model
    (Even if it didn't actually do anything)

--- dotnet/TenmoClient/Models/Transfer.cs
+++ dotnet/TenmoClient/Models/Transfer.cs
@@ -1,12 +1,26 @@
-﻿namespace TenmoClient.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TenmoClient.Models
 {
     public class Transfer
     {
+        [Required(ErrorMessage = "The field TransferID should not be blank.")]
         public int TransferID { get; set; }
+
+        [Required(ErrorMessage = "The field TransferTypeID should not be blank.")]
         public int TransferTypeID { get; set; }
+
+        [Required(ErrorMessage = "The field TransferStatusID should not be blank.")]
         public int TransferStatusID { get; set; }
+
+        [Required(ErrorMessage = "The field AccountFrom should not be blank.")]
         public int AccountFrom { get; set; }
+
+        [Required(ErrorMessage = "The field AccountTo should not be blank.")]
         public int AccountTo { get; set; }
+
+        [Range(0.01, Double.PositiveInfinity, ErrorMessage = "The field Amount should be greater than 0.")]
         public decimal Amount { get; set; }
 
         public string FormattedTransfer
